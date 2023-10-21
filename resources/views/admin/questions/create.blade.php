a76e07ddae3c8e3cc66bbd0f68313df388a2a905
djibrahim <djamaibrahim38@gmail.com>
2023-10-21 17:41:59 +0100

    edit add question

--- resources/views/admin/questions/create.blade.php
+++ resources/views/admin/questions/create.blade.php
@@ -98,16 +98,16 @@
                             <div class="modal-body">
 
                                 <div class="row">
-                                    <div class="col-md-6 form-group">
+                                    <div class="col-6 form-group">
                                         <label>Réponse 1 <span class="text-danger">*</span></label>
                                         <input name="response[]" id="response" type="text" class="form-control form-control" placeholder="entrez la réponse" required>
                                     </div>
-                                    <div class="col-md-1 form-group">
+                                    <div class="col-1 form-group">
                                         <br><br>
                                         <input type="checkbox" class="form-check-input" id="check1" value="0" name="is_correct[]">
 
                                     </div>
-                                    <div class="col-md-4 form-group">
+                                    <div class="col-4 form-group">
                                         <label>Image 1</label>
                                         <div></div>
                                         <label>
@@ -120,16 +120,16 @@
                                 </div>
                                 <hr>
                                <div class="row">
-                                    <div class="col-md-6 form-group">
+                                    <div class="col-6 form-group">
                                         <label>Réponse 2 <span class="text-danger">*</span></label>
                                         <input name="response[]" id="response" type="text" class="form-control form-control" placeholder="entrez la réponse" required>
                                     </div>
-                                    <div class="col-md-1 form-group">
+                                    <div class="col-1 form-group">
                                         <br><br>
                                         <input type="checkbox" class="form-check-input" id="check2" value="1" name="is_correct[]">
 
                                     </div>
-                                    <div class="col-md-4 form-group">
+                                    <div class="col-4 form-group">
                                         <label>Image 2</label>
                                         <div></div>
                                         <label>
@@ -140,16 +140,16 @@
                                </div>
                                 <hr>
                                 <div class="row">
-                                    <div class="col-md-6 form-group">
+                                    <div class="col-6 form-group">
                                         <label>Réponse 3<span class="text-danger">*</span></label>
                                         <input name="response[]" id="response" type="text" class="form-control form-control" placeholder="entrez la réponse" required>
                                     </div>
-                                    <div class="col-md-1 form-group">
+                                    <div class="col-1 form-group">
                                         <br><br>
                                         <input type="checkbox" class="form-check-input" id="check3" value="2" name="is_correct[]">
 
                                     </div>
-                                    <div class="col-md-4 form-group">
+                                    <div class="col-4 form-group">
                                         <label>Image 3</label>
                                         <div></div>
                                         <label>
@@ -160,15 +160,15 @@
                                 </div>
                                 <hr>
                                 <div class="row">
-                                    <div class="col-md-6 form-group">
+                                    <div class="col-6 form-group">
                                         <label>Réponse 4 <span class="text-danger">*</span></label>
                                         <input name="response[]" id="response" type="text" class="form-control form-control" placeholder="entrez la réponse" required>
                                     </div>
-                                    <div class="col-md-1 form-group">
+                                    <div class="col-1 form-group">
                                         <br><br>
                                         <input type="checkbox" class="form-check-input" id="check4" value="3" name="is_correct[]">
                                     </div>
-                                    <div class="col-md-4 form-group">
+                                    <div class="col-4 form-group">
                                         <label>Image 4</label>
                                         <div></div>
                                         <label>
@@ -179,16 +179,16 @@
                                 </div>
                                 <hr>
                                 <div class="row">
-                                    <div class="col-md-6 form-group">
+                                    <div class="col-6 form-group">
                                         <label>Réponse 5 <span class="text-danger">*</span></label>
                                         <input name="response[]" id="response" type="text" class="form-control form-control" placeholder="entrez la réponse" required>
                                     </div>
-                                    <div class="col-md-1 form-group">
+                                    <div class="col-1 form-group">
                                         <br><br>
                                         <input type="checkbox" class="form-check-input" id="check5" value="4" name="is_correct[]">
 
                                     </div>
-                                    <div class="col-md-4 form-group">
+                                    <div class="col-4 form-group">
                                         <label>Image 5</label>
                                         <div></div>
                                          <label>
